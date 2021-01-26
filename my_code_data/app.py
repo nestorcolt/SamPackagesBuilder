a694bd23a4c9fe13b67e22a2b8e433209b431f5f
nestor <nestorcolt@hotmail.com>
2021-01-26 13:26:16 +0100

    simplejson

--- my_code_data/app.py
+++ my_code_data/app.py
@@ -3,18 +3,4 @@ import json
 
 
 def lambda_handler(event, context):
-    try:
-        ip = requests.get("http://checkip.amazonaws.com/")
-        print(f"Request Module working:\n{ip.conten}")
-    except requests.RequestException as e:
-        # Send some context about this error to Lambda Logs
-        print(e)
-        raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            "location": ip.text.replace("\n", "")
-        }),
-    }
+    return None
